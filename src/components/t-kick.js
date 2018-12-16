558afb2abf3d6ee5cfba6a90dd13c00d3c529c51
Abdu Assabri <abdusabri@outlook.com>
2018-12-16 12:41:54 +0100

    Set the approach and do basic handling playing sound on button clicks

--- src/components/t-kick.js
+++ src/components/t-kick.js
@@ -3,12 +3,28 @@ import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import "./t-kick.css";
 
+let audioRef;
+
 const TKick = props => {
   return (
     <div className="kick">
-      <Button variant="contained" style={{ height: "100%" }}>
+      <Button
+        variant="contained"
+        style={{ height: "100%" }}
+        onClick={() => {
+          audioRef.currentTime = 0; // https://stackoverflow.com/questions/13002935/html5-audio-start-over
+          audioRef.play();
+        }}
+      >
         Kick
       </Button>
+      {/* Solution inspired by https://codepen.io/StudentOfJS/pen/dzjPwP/ */}
+      <audio ref={element => (audioRef = element)}>
+        <source
+          src="http://soundbible.com/mp3/Roundhouse Kick-SoundBible.com-1663225804.mp3"
+          type="audio/mpeg"
+        />
+      </audio>
     </div>
   );
 };
