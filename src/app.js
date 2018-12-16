8868b4c72cfa87833a330102e2f1cff242de8a67
Abdu Assabri <abdusabri@outlook.com>
2018-12-16 09:36:56 +0100

    Create ore structure, add container component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./app.css";
+import TSoundsContainer from "./components/t-sounds-container";
 
 class App extends Component {
   render() {
-    return <div className="App">Taekwondo kick sounds!</div>;
+    return (
+      <div className="app">
+        <TSoundsContainer />
+      </div>
+    );
   }
 }
 
---
+++ src/components/t-sounds-container.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class TSoundsContainer extends Component {
+  render() {
+    return <span>Taekwondo kick sounds!</span>;
+  }
+}
+
+export default TSoundsContainer;
