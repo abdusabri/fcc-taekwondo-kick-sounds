1562b96604498a842db14843bf2048edd9f1296b
Abdu Assabri <abdusabri@outlook.com>
2018-12-16 16:33:08 +0100

    Updates

---
+++ precache-manifest.f4c3acda343f69621c0df1c58d8fc124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b410f954cf8531daf0e6",
+    "url": "/fcc-taekwondo-kick-sounds/static/js/runtime~main.b410f954.js"
+  },
+  {
+    "revision": "45daa13754c6e373f40c",
+    "url": "/fcc-taekwondo-kick-sounds/static/js/main.45daa137.chunk.js"
+  },
+  {
+    "revision": "916747988ea22d9127af",
+    "url": "/fcc-taekwondo-kick-sounds/static/js/1.91674798.chunk.js"
+  },
+  {
+    "revision": "45daa13754c6e373f40c",
+    "url": "/fcc-taekwondo-kick-sounds/static/css/main.d29b7613.chunk.css"
+  },
+  {
+    "revision": "fc6a15063b8176c701a990829b35bcab",
+    "url": "/fcc-taekwondo-kick-sounds/index.html"
+  }
+];
